7bd9bfbea5de80d011594cc7a3613327858cc6ef
Evan Brumley <evan.brumley@wspdigital.com>
2022-12-01 18:26:42 +1100

    Add solution for Day 1

---
+++ day1/solution1.py
@@ -0,0 +1,37 @@
+class CalorieCounter:
+    def __init__(self, elves):
+        self.elves = elves
+
+    @classmethod
+    def from_input(cls, lines):
+        elves = []
+        current_elf = []
+
+        for line in lines:
+            if not line:
+                elves.append(current_elf)
+                current_elf = []
+            else:
+                current_elf.append(int(line))
+
+        if current_elf:
+            elves.append(current_elf)
+
+        return CalorieCounter(elves)
+
+    @property
+    def elf_totals(self):
+        return [sum(elf) for elf in self.elves]
+
+
+def main():
+    with open("input", "r") as f:
+        lines = f.read().splitlines()
+
+    counter = CalorieCounter.from_input(lines)
+
+    print(max(counter.elf_totals))
+
+
+if __name__ == "__main__":
+    main()
